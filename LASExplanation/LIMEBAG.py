c9398beac4bcf397e38b76799817f3bfcacaabbd
kpeng2019 <kpeng@ncsu.edu>
2020-05-29 12:54:00 -0400

    add 2 demo functions

--- LASExplanation/LIMEBAG.py
+++ LASExplanation/LIMEBAG.py
@@ -2,7 +2,11 @@ import pandas as pd
 import numpy as np
 import lime.lime_tabular
 import sk as sk
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import os
 
 
 class LIMEBAG():
@@ -122,4 +126,60 @@ class LIMEBAG():
             sk.main(file=f1,higher=higher,latex=latex)
         else:
             raise ValueError("Expected type to be either values or ranks.")
-        return 0
+        return True
+
+    def __demo1__(self):
+        file = os.path.join(os.getcwd(), 'camel-1.2.csv')
+        df = pd.read_csv(os.path.normpath(file))
+        # demo using a software defect prediction dataset
+        for i in range(0, df.shape[0]):
+            if df.iloc[i, -1] > 0:
+                df.iloc[i, -1] = 1
+            else:
+                df.iloc[i, -1] = 0
+        X = df.iloc[:, :-1]
+        y = df.iloc[:, -1]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+        sc = MinMaxScaler()
+        X_train = pd.DataFrame(sc.fit_transform(X_train), columns=X_train.columns).copy()
+        X_test = pd.DataFrame(sc.fit_transform(X_test), columns=X_train.columns).copy()
+        clf = RandomForestClassifier()
+        clf.fit(X_train, y_train)
+        bag = LIMEBAG(clf=clf, y_train=y_train, X_test=X_test, X_train=X_train, K=1)
+        # leave sensitive as none if the data has no fairness concerns
+        for i in range(len(X_train.columns)):
+            print('Index', i, ':', X_train.columns[i])
+        ranks, rankvals = bag.explain()
+        print("Feature importance ranks of all test data returned by LIMEBAG:")
+        print(ranks)
+        print("Feature importance weights of all test data returned by LIMEBAG:")
+        print(rankvals)
+        return True
+
+    def __demo2__(self):
+        file = os.path.join(os.getcwd(), 'camel-1.2.csv')
+        df = pd.read_csv(file)
+        # demo using a software defect prediction dataset
+        for i in range(0, df.shape[0]):
+            if df.iloc[i, -1] > 0:
+                df.iloc[i, -1] = 1
+            else:
+                df.iloc[i, -1] = 0
+        X = df.iloc[:, :-1]
+        y = df.iloc[:, -1]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+        sc = MinMaxScaler()
+        X_train = pd.DataFrame(sc.fit_transform(X_train), columns=X_train.columns).copy()
+        X_test = pd.DataFrame(sc.fit_transform(X_test), columns=X_train.columns).copy()
+        clf = RandomForestClassifier()
+        clf.fit(X_train, y_train)
+        bag = LIMEBAG(clf=clf, y_train=y_train, X_test=X_test, X_train=X_train, K=1)
+        # leave sensitive as none if the data has no fairness concerns
+        for i in range(len(X_train.columns)):
+            print('Index', i, ':', X_train.columns[i])
+        ranks, rankvals = bag.explain()
+        print('*' * 10, "Summary of feature importance weights", '*' * 10)
+        bag.find_rank(type='values', higher=True, latex=True)
+        print('*' * 10, "Summary of feature importance ranks", '*' * 10)
+        bag.find_rank(type='ranks', latex=True)
+        return True
