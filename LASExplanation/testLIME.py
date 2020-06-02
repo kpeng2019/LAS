4f278cbfd995b570cf370ea167baffb34b06ae4d
kpeng2019 <kpeng@ncsu.edu>
2020-06-02 10:25:34 -0400

    update tests

--- LASExplanation/testLIME.py
+++ LASExplanation/testLIME.py
@@ -1,7 +1,7 @@
 import subprocess
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from LIMEBAG import *
+from LASExplanation.LIMEBAG import *
 import os
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import MinMaxScaler
--- LASExplanation/testSHAP.py
+++ LASExplanation/testSHAP.py
@@ -1,7 +1,7 @@
 import subprocess
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from SHAP import *
+from LASExplanation.SHAP import *
 import os
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import MinMaxScaler
